9c85f774a4b7f80d0e5dbbd33fa524f56b55eeb1
David <zhangdavid5915@hotmail.com.au>
2022-03-09 16:31:06 +1100

    Made a rotating shape

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # 3D-Portfoilio
+
+- Tutorial Video
+- https://www.youtube.com/watch?v=Q7AOvWpIVHU 
+
+- Documentation
+- https://threejs.org/docs/#api/en/geometries/RingGeometry 
+
--- main.js
+++ main.js
@@ -1,6 +1,36 @@
 import './style.css'
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+import * as THREE from 'three';
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+const renderer = new THREE.WebGL1Renderer({
+  canvas: document.querySelector('#bg'),
+});
+
+renderer.setPixelRatio( window.devicePixelRatio );
+renderer.setSize( window.innerWidth, window.innerHeight)
+camera.position.setZ(30);
+
+renderer.render( scene, camera ); 
+
+const geometry = new THREE.DodecahedronGeometry( 10 );
+const material = new THREE.MeshBasicMaterial( { colour: 0xFF6347, wireframe: true } );
+const dodecahedron = new THREE.Mesh( geometry, material );
+
+scene.add(dodecahedron)
+
+function animate() {
+  requestAnimationFrame( animate );
+
+  dodecahedron.rotation.x += 0.01;
+  dodecahedron.rotation.y += 0.005;
+  dodecahedron.rotation.z += 0.01;
+
+
+  renderer.render( scene, camera );
+}
+
+animate();
